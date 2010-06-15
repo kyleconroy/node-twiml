2de4c0e4b15c99f857e28fdc14a0cdf64f099ea7
Kyle Conroy <kyle.j.conroy@gmail.com>
2010-06-15 16:55:38 -0700

    First implementation, no unit tests

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 require('./twiml');
+var System = require('sys');
 
-twiml(function(){
+System.puts(twiml(function(){
 
     say("Hello World")        // Adds a say object to the response
         .voice("male")         // Changes that object;b
@@ -14,7 +15,23 @@ twiml(function(){
             .voice("french")
             
         play("http://www.url-to-no-where")
+        
+        dial("5454567890")
 
     })
+    
+    record().method("POST")
+    
+    dial(function(){
+        number("345234123")
+            .sendDigits("2341")
+    })
+    
+    dial(function(){
+        conference("MyRoomName")
+    })
+    
+    pause().length(10)
+    hangup()
 
-})
+}));
--- twiml.js
+++ twiml.js
@@ -17,12 +17,12 @@ Verb.prototype.set = function(attribute, val){
     this.attrs.push(attr);
 };
 
-Verb.prototype.render = function(output){
-    output += "<" + this.name;
+Verb.prototype.render = function(){
+    var output = "<" + this.name;
     
     for (var i=0; i < this.attrs.length; i++) {
         output += " " + this.attrs[i].name + '="';
-        output += " " + this.attrs[i].value + '"';
+        output += this.attrs[i].value + '"';
     };
     
     output += ">"
@@ -30,76 +30,285 @@ Verb.prototype.render = function(output){
     if (this.body !== undefined)
         output += this.body
     
-    for (var i=0; i < this._verbs.length; i++) {
-        output = this.verbs[i].render(output);
+    for (var i=0; i < this.verbs.length; i++) {
+        output += this.verbs[i].render();
     };
     
     output += "</" + this.name + ">";
     return output;
 };
 
-var Say = function(text) {
-    this._body = text;
-    this._voice = false;
-    this._language = false;
-};
-
-Say.prototype.voice = function(v){
-    this._voice = v;
-    return this;
-};
-
-Say.prototype.language = function(lang) {
-    this._language = lang;
-    return this;
-};
-
-Say.prototype.render = function(lang) {
-    System.puts("[Say]");
-    System.puts(this._body);
-    System.puts("[/Say]");
+var Say = function(body) {
+    this.body = body;
+    this.name = "Say";
 }
-
-var Play = function(url) {
-    this._url = url;
-    this._loop = false;
-};
-
-Play.prototype.loop = function(num){
-    this._loop = num;
+ 
+Say.prototype = new Verb();
+ 
+Say.prototype.voice = function(val) {
+    this.set("voice", val);
     return this;
 };
-
-Play.prototype.render = function(lang) {
-    System.puts("[Play");
-    
-    if (this._loop)
-        System.puts(" loop=\"" + this._loop + "\"");
-    
-    System.puts("]")
-    System.puts(this._url);
-    System.puts("[/Play]");
+ 
+Say.prototype.language = function(val) {
+    this.set("language", val);
+    return this;
 };
-
+ 
+Say.prototype.loop = function(val) {
+    this.set("loop", val);
+    return this;
+};
+ 
+var Play = function(body) {
+    this.body = body;
+    this.name = "Play";
+}
+ 
+Play.prototype = new Verb();
+ 
+Play.prototype.loop = function(val) {
+    this.set("loop", val);
+    return this;
+};
+ 
+var Gather = function(verbs) {
+    this.verbs = verbs;
+    this.name = "Gather";
+}
+ 
+Gather.prototype = new Verb();
+ 
+Gather.prototype.action = function(val) {
+    this.set("action", val);
+    return this;
+};
+ 
+Gather.prototype.method = function(val) {
+    this.set("method", val);
+    return this;
+};
+ 
+Gather.prototype.timeout = function(val) {
+    this.set("timeout", val);
+    return this;
+};
+ 
+Gather.prototype.finishOnKey = function(val) {
+    this.set("finishOnKey", val);
+    return this;
+};
+ 
+Gather.prototype.numDigits = function(val) {
+    this.set("numDigits", val);
+    return this;
+};
+ 
 var Record = function() {
-    this._action = false;
-    this._method = false;
-    this._timeout = false;
-    this._finishOnKey = false;
-    this._maxLength = false;
+    this.name = "Record";
 }
-
-var Gather = function(vrbs) {
-    this._verbs = vrbs;
+ 
+Record.prototype = new Verb();
+ 
+Record.prototype.action = function(val) {
+    this.set("action", val);
+    return this;
 };
-
-Gather.prototype.render = function(lang) {
-    System.puts("[Gather]");
-    for (var i=0; i < this._verbs.length; i++) {
-        this._verbs[i].render();
-    };
-    System.puts("[/Gather]");
+ 
+Record.prototype.method = function(val) {
+    this.set("method", val);
+    return this;
+};
+ 
+Record.prototype.timeout = function(val) {
+    this.set("timeout", val);
+    return this;
+};
+ 
+Record.prototype.finishOnKey = function(val) {
+    this.set("finishOnKey", val);
+    return this;
+};
+ 
+Record.prototype.maxLength = function(val) {
+    this.set("maxLength", val);
+    return this;
+};
+ 
+Record.prototype.transcribe = function(val) {
+    this.set("transcribe", val);
+    return this;
+};
+ 
+Record.prototype.transcribeCallback = function(val) {
+    this.set("transcribeCallback", val);
+    return this;
+};
+ 
+Record.prototype.playBeep = function(val) {
+    this.set("playBeep", val);
+    return this;
+};
+ 
+var Sms = function(body) {
+    this.body = body;
+    this.name = "Sms";
 }
+ 
+Sms.prototype = new Verb();
+ 
+Sms.prototype.action = function(val) {
+    this.set("action", val);
+    return this;
+};
+ 
+Sms.prototype.method = function(val) {
+    this.set("method", val);
+    return this;
+};
+ 
+Sms.prototype.to = function(val) {
+    this.set("to", val);
+    return this;
+};
+ 
+Sms.prototype.from = function(val) {
+    this.set("from", val);
+    return this;
+};
+ 
+Sms.prototype.statusCallback = function(val) {
+    this.set("statusCallback", val);
+    return this;
+};
+ 
+var Dial = function(cons) {
+    if (typeof cons == "string") {
+        this.body = cons;
+    } else {
+        this.verbs = cons;
+    }
+    this.name = "Dial";
+}
+ 
+Dial.prototype = new Verb();
+ 
+Dial.prototype.action = function(val) {
+    this.set("action", val);
+    return this;
+};
+ 
+Dial.prototype.method = function(val) {
+    this.set("method", val);
+    return this;
+};
+ 
+Dial.prototype.timeout = function(val) {
+    this.set("timeout", val);
+    return this;
+};
+ 
+Dial.prototype.hangupOnStar = function(val) {
+    this.set("hangupOnStar", val);
+    return this;
+};
+ 
+Dial.prototype.timeLimit = function(val) {
+    this.set("timeLimit", val);
+    return this;
+};
+ 
+Dial.prototype.callerId = function(val) {
+    this.set("callerId", val);
+    return this;
+};
+ 
+var Redirect = function(body) {
+    this.body = body;
+    this.name = "Redirect";
+}
+ 
+Redirect.prototype = new Verb();
+ 
+Redirect.prototype.method = function(val) {
+    this.set("method", val);
+    return this;
+};
+ 
+var Number = function(body) {
+    this.body = body;
+    this.name = "Number";
+}
+ 
+Number.prototype = new Verb();
+ 
+Number.prototype.url = function(val) {
+    this.set("url", val);
+    return this;
+};
+ 
+Number.prototype.sendDigits = function(val) {
+    this.set("sendDigits", val);
+    return this;
+};
+ 
+var Conference = function(body) {
+    this.body = body;
+    this.name = "Conference";
+}
+ 
+Conference.prototype = new Verb();
+ 
+Conference.prototype.muted = function(val) {
+    this.set("muted", val);
+    return this;
+};
+ 
+Conference.prototype.beep = function(val) {
+    this.set("beep", val);
+    return this;
+};
+ 
+Conference.prototype.startConferenceOnEnter = function(val) {
+    this.set("startConferenceOnEnter", val);
+    return this;
+};
+ 
+Conference.prototype.endConferenceOnExit = function(val) {
+    this.set("endConferenceOnExit", val);
+    return this;
+};
+ 
+Conference.prototype.waitUrl = function(val) {
+    this.set("waitUrl", val);
+    return this;
+};
+ 
+Conference.prototype.waitMethod = function(val) {
+    this.set("waitMethod", val);
+    return this;
+};
+ 
+var Pause = function() {
+    this.name = "Pause";
+}
+ 
+Pause.prototype = new Verb();
+ 
+Pause.prototype.length = function(val) {
+    this.set("length", val);
+    return this;
+};
+ 
+var Hangup = function() {
+    this.name = "Hangup";
+}
+ 
+Hangup.prototype = new Verb();
+ 
+
+
+
 
 
 var verbs = [];
@@ -111,9 +320,14 @@ var addVerb = function(verb){
     
 GLOBAL.twiml = function(f) {
     f();
+    var output = "<Response>";
+    
     for (var i=0; i < verbs.length; i++) {
-        verbs[i].render();
+        output += verbs[i].render();
     };
+    
+    output += "</Response>";
+    return output;
 };
 
 GLOBAL.say = function(body) {
@@ -136,5 +350,44 @@ GLOBAL.gather = function(f){
     return verb;
 };
 
+GLOBAL.redirect = function(body) {
+    return addVerb(new Redirect(body));
+};
+
+GLOBAL.conference = function(body) {
+    return addVerb(new Conference(body));
+};
+
+GLOBAL.number = function(body) {
+    return addVerb(new Number(body));
+}
+
+GLOBAL.record = function(body) {
+    return addVerb(new Record());
+};
+
+GLOBAL.hangup = function() {
+    return addVerb(new Hangup());
+};
+
+GLOBAL.dial = function(f) {
+    if (typeof f == "string") {
+        return addVerb(new Dial(f));
+    } else {
+        var temp = verbs;
+        verbs = [];
+
+        f();
+
+        verb = new Dial(verbs);
+        verbs = temp;
+        verbs.push(verb);
+        return verb;
+    }
+};
+
+GLOBAL.pause = function(body) {
+    return addVerb(new Pause(body));
+}
   
     
